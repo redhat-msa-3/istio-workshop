4ce277114fc022675fa12c4b8c3084f6b5daaff6
Rafael Benevides <rafabene@gmail.com>
2018-01-02 21:43:16 -0200

    Login as developer after

--- 1-deploy-microservices/env-init.sh
+++ 1-deploy-microservices/env-init.sh
@@ -20,4 +20,5 @@ ssh root@host01 "oc expose svc grafana"
 ssh root@host01 "oc expose svc prometheus"
 
 ssh root@host01 "oc process -f https://raw.githubusercontent.com/jaegertracing/jaeger-openshift/master/all-in-one/jaeger-all-in-one-template.yml | oc create -f -"
+ssh root@host01 "oc login -u developer -p developer"
 ssh root@host01 "oc project default"
