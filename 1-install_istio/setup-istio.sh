dbdbf87d45066642db2164f7b86c2285c1b1d66d
Rafael Benevides <rafabene@gmail.com>
2018-01-02 20:31:06 -0200

    Change courseData

--- 1-install_istio/setup-istio.sh
+++ 1-install_istio/setup-istio.sh
@@ -1,24 +1,24 @@
-oc adm policy add-cluster-role-to-user admin developer > /dev/null 2>&1
-oc login -u system:admin > /dev/null 2>&1
-oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system > /dev/null 2>&1
-oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system > /dev/null 2>&1
-oc adm policy add-scc-to-user anyuid -z default -n istio-system > /dev/null 2>&1
-wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/ > /dev/null 2>&1
-cd /root/ > /dev/null 2>&1
-tar -zxvf istio-0.4.0-linux.tar.gz > /dev/null 2>&1
-cd /root/istio-0.4.0/ > /dev/null 2>&1
-oc apply -f install/kubernetes/istio.yaml > /dev/null 2>&1
-oc apply -f install/kubernetes/addons/prometheus.yaml > /dev/null 2>&1
-oc apply -f install/kubernetes/addons/grafana.yaml > /dev/null 2>&1
-oc apply -f install/kubernetes/addons/servicegraph.yaml > /dev/null 2>&1
-
-oc project istio-system  > /dev/null 2>&1
-
-oc expose svc istio-ingress  > /dev/null 2>&1
-oc expose svc servicegraph > /dev/null 2>&1
-oc expose svc grafana > /dev/null 2>&1
-oc expose svc prometheus > /dev/null 2>&1
-
-oc process -f https://raw.githubusercontent.com/jaegertracing/jaeger-openshift/master/all-in-one/jaeger-all-in-one-template.yml | oc create -f -  > /dev/null 2>&1
+oc adm policy add-cluster-role-to-user admin developer 
+oc login -u system:admin
+oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system
+oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system
+oc adm policy add-scc-to-user anyuid -z default -n istio-system
+wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/
 cd /root/
+tar -zxvf istio-0.4.0-linux.tar.gz
+cd /root/istio-0.4.0/
+oc apply -f install/kubernetes/istio.yaml
+oc apply -f install/kubernetes/addons/prometheus.yaml
+oc apply -f install/kubernetes/addons/grafana.yaml
+oc apply -f install/kubernetes/addons/servicegraph.yaml
+
+oc project istio-system 
+
+oc expose svc istio-ingress 
+oc expose svc servicegraph
+oc expose svc grafana
+oc expose svc prometheus
+
+oc process -f https://raw.githubusercontent.com/jaegertracing/jaeger-openshift/master/all-in-one/jaeger-all-in-one-template.yml | oc create -f -
+
 echo "Istio installed"
