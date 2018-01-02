2dae8b9105eccf13e056b2292083a3c46fae8a79
Rafael Benevides <rafabene@gmail.com>
2018-01-02 21:29:48 -0200

    Change scenario background script to ssh

--- 1-deploy-microservices/env-init.sh
+++ 1-deploy-microservices/env-init.sh
@@ -1,12 +1,12 @@
-oc adm policy add-cluster-role-to-user admin developer 
-oc login -u system:admin
-oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system
-oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system
-oc adm policy add-scc-to-user anyuid -z default -n istio-system
-wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/
-cd /root/
-tar -zxvf istio-0.4.0-linux.tar.gz
-cd /root/istio-0.4.0/
+ssh root@host01 "oc adm policy add-cluster-role-to-user admin developer "
+ssh root@host01 "oc login -u system:admin"
+ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
+ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system"
+ssh root@host01 "oc adm policy add-scc-to-user anyuid -z default -n istio-system"
+ssh root@host01 "wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /usr/src/"
+
+ssh root@host01 "tar -zxvf /usr/src/istio-0.4.0-linux.tar.gz"
+
 oc apply -f install/kubernetes/istio.yaml
 oc apply -f install/kubernetes/addons/prometheus.yaml
 oc apply -f install/kubernetes/addons/grafana.yaml
