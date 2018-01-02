50187888afe2b89ae19b820a6689ce5d39fe1d25
Rafael Benevides <rafabene@gmail.com>
2018-01-02 21:33:46 -0200

    Change scenario background script to include more commands

--- 1-deploy-microservices/env-init.sh
+++ 1-deploy-microservices/env-init.sh
@@ -5,20 +5,20 @@ ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-egress-service-ac
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z default -n istio-system"
 ssh root@host01 "wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /usr/src/"
 
-ssh root@host01 "tar -zxvf /usr/src/istio-0.4.0-linux.tar.gz"
+ssh root@host01 "tar -zxvf /usr/src/istio-0.4.0-linux.tar.gz -C /usr/src" 
 
-oc apply -f install/kubernetes/istio.yaml
-oc apply -f install/kubernetes/addons/prometheus.yaml
-oc apply -f install/kubernetes/addons/grafana.yaml
-oc apply -f install/kubernetes/addons/servicegraph.yaml
+ssh root@host01 "oc apply -f /usr/src/istio-0.4.0/install/kubernetes/istio.yaml"
+ssh root@host01 "oc apply -f /usr/src/istio-0.4.0/install/kubernetes/addons/prometheus.yaml"
+ssh root@host01 "oc apply -f /usr/src/istio-0.4.0/install/kubernetes/addons/grafana.yaml"
+ssh root@host01 "oc apply -f /usr/src/istio-0.4.0/install/kubernetes/addons/servicegraph.yaml"
 
-oc project istio-system 
+ssh root@host01 "oc project istio-system"
 
-oc expose svc istio-ingress 
-oc expose svc servicegraph
-oc expose svc grafana
-oc expose svc prometheus
+ssh root@host01 "oc expose svc istio-ingress"
+ssh root@host01 "oc expose svc servicegraph"
+ssh root@host01 "oc expose svc grafana"
+ssh root@host01 "oc expose svc prometheus"
 
-oc process -f https://raw.githubusercontent.com/jaegertracing/jaeger-openshift/master/all-in-one/jaeger-all-in-one-template.yml | oc create -f -
+ssh root@host01 "oc process -f https://raw.githubusercontent.com/jaegertracing/jaeger-openshift/master/all-in-one/jaeger-all-in-one-template.yml | oc create -f -"
 
 echo "Istio installed"
