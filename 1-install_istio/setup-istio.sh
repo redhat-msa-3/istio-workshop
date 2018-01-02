a8718ba1e075612377da2c2e0f4dbc3dee696b5d
Rafael Benevides <rafabene@gmail.com>
2018-01-02 20:15:24 -0200

    Setup Istio with everything

--- 1-install_istio/setup-istio.sh
+++ 1-install_istio/setup-istio.sh
@@ -6,4 +6,17 @@ oc adm policy add-scc-to-user anyuid -z default -n istio-system
 wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/
 cd /root/
 tar -zxvf istio-0.4.0-linux.tar.gz
+cd /root/istio-0.4.0/
+oc apply -f install/kubernetes/istio.yaml
+oc apply -f install/kubernetes/addons/prometheus.yaml
+oc apply -f install/kubernetes/addons/grafana.yaml
+oc apply -f install/kubernetes/addons/servicegraph.yaml
 
+oc project istio-system 
+
+oc expose svc istio-ingress 
+oc expose svc servicegraph
+oc expose svc grafana
+oc expose svc prometheus
+
+oc process -f https://raw.githubusercontent.com/jaegertracing/jaeger-openshift/master/all-in-one/jaeger-all-in-one-template.yml | oc create -f -
