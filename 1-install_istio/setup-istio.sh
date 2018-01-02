69454d8b322129304a51bbfb89778804c13e30c7
Rafael Benevides <rafabene@gmail.com>
2018-01-02 19:51:58 -0200

    Setup Istio 2

--- 1-install_istio/setup-istio.sh
+++ 1-install_istio/setup-istio.sh
@@ -1,9 +1,8 @@
-oc adm policy add-cluster-role-to-user admin developer
-oc login -u system:admin
-oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system
-oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system
-oc adm policy add-scc-to-user anyuid -z default -n istio-system
-wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/
-cd /root/
-tar -zxvf istio-0.4.0-linux.tar.gz
+ssh root@host01 "oc adm policy add-cluster-role-to-user admin developer"
+ssh root@host01 "oc login -u system:admin'"
+ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
+ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system"
+ssh root@host01 "oc adm policy add-scc-to-user anyuid -z default -n istio-system"
+ssh root@host01 "wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/"
+ssh root@host01 "tar -zxvf istio-0.4.0-linux.tar.gz"
 
