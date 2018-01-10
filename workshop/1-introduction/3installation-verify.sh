1e1b30f4c81c2027465a0eb75c69bf85b16e37ab
Rafael Benevides <rafabene@gmail.com>
2018-01-10 19:33:59 -0200

    Update verification script

--- workshop/1-introduction/3installation-verify.sh
+++ workshop/1-introduction/3installation-verify.sh
@@ -1,1 +1,1 @@
-export PATH=$PATH:/root/istio-0.4.0/bin/ && ( /root/istio-0.4.0/bin/istioctl version >> /dev/null ) && echo "done"
+( /root/istio-0.4.0/bin/istioctl version >> /dev/null ) && echo \"done\"
