b5a3f02ed0f04fec819b0476c998f6be207c6cc5
Rafael Benevides <rafabene@gmail.com>
2018-01-11 11:20:50 -0200

    Added assets folder

--- workshop/3-monitoring-tracing/env-init.sh
+++ workshop/3-monitoring-tracing/env-init.sh
@@ -4,7 +4,6 @@ ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer"
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system"
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z default -n istio-system"
-ssh root@host01 "wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/"
 
 ssh root@host01 "tar -zxvf /root/istio-0.4.0-linux.tar.gz -C /root" 
 
@@ -14,7 +13,6 @@ ssh root@host01 "oc expose svc istio-ingress"
 ssh root@host01 "oc login -u developer -p developer"
 
 ssh root@host01 "yum install java-1.8.0-openjdk-devel tree -y"
-ssh root@host01 "wget http://www.eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz -P /usr/src"
 ssh root@host01 "tar xzf /usr/src/apache-maven-3.3.9-bin.tar.gz -C /usr/src"
 ssh root@host01 "rm -rf /usr/src/apache-maven-3.3.9-bin.tar.gz"
 ssh root@host01 "mkdir /usr/local/maven"
