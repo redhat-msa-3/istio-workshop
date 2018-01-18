b6320da57fddc95be16a1cd48494e41f3f4c704e
Rafael Benevides <rafabene@gmail.com>
2018-01-17 23:48:50 -0500

    Try to use editor 5

--- workshop/4-simple-routerules/env-init.sh
+++ workshop/4-simple-routerules/env-init.sh
@@ -2,7 +2,9 @@ ssh root@host01 "oc login -u system:admin"
 
 ssh root@host01 "oc new-project tutorial"
 ssh root@host01 "oc adm policy add-scc-to-user privileged -z default -n tutorial"
-ssh root@host01 "git clone https://github.com/redhat-developer-demos/istio-tutorial /root/istio-tutorial"
+ssh root@host01 "git clone https://github.com/redhat-developer-demos/istio-tutorial /root/tmp"
+ssh root@host01 "mv /root/tmp/* istio-tutorial/"
+ssh root@host01 "rm -rf /root/tmp"
 
 ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer"
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
