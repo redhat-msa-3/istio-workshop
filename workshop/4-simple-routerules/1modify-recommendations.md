a6d9b137decf676089e3a080394e740009437b02
Rafael Benevides <rafabene@gmail.com>
2018-01-17 23:15:24 -0500

    Try to use editor

--- workshop/4-simple-routerules/1modify-recommendations.md
+++ workshop/4-simple-routerules/1modify-recommendations.md
@@ -4,6 +4,6 @@ We can experiment with Istio routing rules by making a change to Recommendations
 
 We can experiment with Istio routing rules by making a change to RecommendationsController.java like
 
-`System.out.println("Big Red Dog v2 " + cnt);
-
-return "Clifford v2 " + cnt;`
+    System.out.println("Big Red Dog v2 " + cnt);
+     
+    return "Clifford v2 " + cnt;
--- workshop/4-simple-routerules/env-init.sh
+++ workshop/4-simple-routerules/env-init.sh
@@ -1,4 +1,9 @@
 ssh root@host01 "oc login -u system:admin"
+
+ssh root@host01 "oc new-project tutorial"
+ssh root@host01 "oc adm policy add-scc-to-user privileged -z default -n tutorial"
+ssh root@host01 "git clone https://github.com/redhat-developer-demos/istio-tutorial /root/istio-tutorial"
+
 ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer"
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system"
@@ -20,9 +25,6 @@ ssh root@host01 "mkdir /usr/local/maven"
 ssh root@host01 "mv /usr/src/apache-maven-3.3.9/ /usr/local/maven/"
 ssh root@host01 "alternatives --install /usr/bin/mvn mvn /usr/local/maven/apache-maven-3.3.9/bin/mvn 1"
 
-ssh root@host01 "oc new-project tutorial"
-ssh root@host01 "oc adm policy add-scc-to-user privileged -z default -n tutorial"
-ssh root@host01 "git clone https://github.com/redhat-developer-demos/istio-tutorial /root/istio-tutorial"
 
 ssh root@host01 "mvn package -f /root/istio-tutorial/customer/ -DskipTests"
 ssh root@host01 "mvn package -f /root/istio-tutorial/recommendations/ -DskipTests"
