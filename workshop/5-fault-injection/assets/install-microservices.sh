f10c0779110c4098bdfac49a303811a03fe99489
Rafael Benevides <rafabene@gmail.com>
2018-01-20 13:19:36 -0500

    Deploy microservices manually

---
+++ workshop/5-fault-injection/assets/install-microservices.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+oc login -u system:admin
+
+git clone https://github.com/redhat-developer-demos/istio-tutorial
+cp -Rvf istio-tutorial/recommendations/ istio-tutorial/recommendations-v2
+git apply /root/recommendations-v2.diff --include /root/istio-tutorial
+rm /root/recommendations-v2.diff
+oc new-project tutorial
+oc adm policy add-scc-to-user privileged -z default -n tutorial
+
+## Install Java
+yum install java-1.8.0-openjdk-devel tree -y
+
+#Install Maven
+wget http://www.eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz -P /usr/src
+tar xzf /usr/src/apache-maven-3.3.9-bin.tar.gz -C /usr/src
+rm -rf /usr/src/apache-maven-3.3.9-bin.tar.gz
+mkdir /usr/local/maven
+mv /usr/src/apache-maven-3.3.9/ /usr/local/maven/
+alternatives --install /usr/bin/mvn mvn /usr/local/maven/apache-maven-3.3.9/bin/mvn 1
+
+#Package Projects
+mvn package -f /root/istio-tutorial/customer/ -DskipTests
+mvn package -f /root/istio-tutorial/recommendations/ -DskipTests
+mvn package -f /root/istio-tutorial/recommendations-v2/ -DskipTests
+mvn package -f /root/istio-tutorial/preferences/ -DskipTests
+
+
+#Docker build
+docker build -q -t example/customer /root/istio-tutorial/customer/
+docker build -q -t example/preferences /root/istio-tutorial/preferences/
+docker build -q -t example/recommendations:v1 /root/istio-tutorial/recommendations/
+docker build -q -t example/recommendations:v2 /root/istio-tutorial/recommendations-v2/
+
+#Deploy to OpenShift
+oc apply -f <(/root/istio-0.4.0/bin/istioctl kube-inject -f /root/istio-tutorial/customer/src/main/kubernetes/Deployment.yml) -n tutorial
+oc apply -f <(/root/istio-0.4.0/bin/istioctl kube-inject -f /root/istio-tutorial/preferences/src/main/kubernetes/Deployment.yml) -n tutorial
+oc apply -f <(/root/istio-0.4.0/bin/istioctl kube-inject -f /root/istio-tutorial/recommendations/src/main/kubernetes/Deployment.yml) -n tutorial
+oc apply -f <(/root/istio-0.4.0/bin/istioctl kube-inject -f /root/istio-tutorial/kubernetesfiles/recommendations_v2_deployment.yml) -n tutorial
+
+oc create -f /root/istio-tutorial/customer/src/main/kubernetes/Service.yml
+oc create -f /root/istio-tutorial/preferences/src/main/kubernetes/Service.yml
+oc create -f /root/istio-tutorial/recommendations/src/main/kubernetes/Service.yml
+
+oc expose service customer -n tutorial
+
+
--- workshop/5-fault-injection/env-init.sh
+++ workshop/5-fault-injection/env-init.sh
@@ -1,12 +1,5 @@
 ssh root@host01 "oc login -u system:admin"
 
-ssh root@host01 "git clone https://github.com/redhat-developer-demos/istio-tutorial"
-ssh root@host01 "cp -Rvf istio-tutorial/recommendations/ istio-tutorial/recommendations-v2"
-ssh root@host01 "git apply /root/recommendations-v2.diff --include /root/istio-tutorial"
-ssh root@host01 "rm /root/recommendations-v2.diff"
-ssh root@host01 "oc new-project tutorial"
-ssh root@host01 "oc adm policy add-scc-to-user privileged -z default -n tutorial"
-
 ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer"
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system"
@@ -19,34 +12,5 @@ ssh root@host01 "oc apply -f /root/istio-0.4.0/install/kubernetes/istio.yaml"
 
 ssh root@host01 "oc expose svc istio-ingress -n istio-system"
 ssh root@host01 "oc login -u developer -p developer"
-
-ssh root@host01 "yum install java-1.8.0-openjdk-devel tree -y"
-ssh root@host01 "wget http://www.eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz -P /usr/src"
-ssh root@host01 "tar xzf /usr/src/apache-maven-3.3.9-bin.tar.gz -C /usr/src"
-ssh root@host01 "rm -rf /usr/src/apache-maven-3.3.9-bin.tar.gz"
-ssh root@host01 "mkdir /usr/local/maven"
-ssh root@host01 "mv /usr/src/apache-maven-3.3.9/ /usr/local/maven/"
-ssh root@host01 "alternatives --install /usr/bin/mvn mvn /usr/local/maven/apache-maven-3.3.9/bin/mvn 1"
-
-ssh root@host01 "mvn package -f /root/istio-tutorial/customer/ -DskipTests"
-ssh root@host01 "mvn package -f /root/istio-tutorial/recommendations/ -DskipTests"
-ssh root@host01 "mvn package -f /root/istio-tutorial/recommendations-v2/ -DskipTests"
-ssh root@host01 "mvn package -f /root/istio-tutorial/preferences/ -DskipTests"
-
-ssh root@host01 "docker build -q -t example/customer /root/istio-tutorial/customer/"
-ssh root@host01 "docker build -q -t example/preferences /root/istio-tutorial/preferences/"
-ssh root@host01 "docker build -q -t example/recommendations:v1 /root/istio-tutorial/recommendations/"
-ssh root@host01 "docker build -q -t example/recommendations:v2 /root/istio-tutorial/recommendations-v2/"
-
-ssh root@host01 "oc apply -f <(/root/istio-0.4.0/bin/istioctl kube-inject -f /root/istio-tutorial/customer/src/main/kubernetes/Deployment.yml) -n tutorial"
-ssh root@host01 "oc apply -f <(/root/istio-0.4.0/bin/istioctl kube-inject -f /root/istio-tutorial/preferences/src/main/kubernetes/Deployment.yml) -n tutorial"
-ssh root@host01 "oc apply -f <(/root/istio-0.4.0/bin/istioctl kube-inject -f /root/istio-tutorial/recommendations/src/main/kubernetes/Deployment.yml) -n tutorial"
-ssh root@host01 "oc apply -f <(/root/istio-0.4.0/bin/istioctl kube-inject -f /root/istio-tutorial/kubernetesfiles/recommendations_v2_deployment.yml) -n tutorial"
-
-ssh root@host01 "oc create -f /root/istio-tutorial/customer/src/main/kubernetes/Service.yml"
-ssh root@host01 "oc create -f /root/istio-tutorial/preferences/src/main/kubernetes/Service.yml"
-ssh root@host01 "oc create -f /root/istio-tutorial/recommendations/src/main/kubernetes/Service.yml"
-
-ssh root@host01 "oc expose service customer -n tutorial"
 
 ssh root@host01 "wget https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail -P /usr/local/bin && chmod +x /usr/local/bin/kubetail"
