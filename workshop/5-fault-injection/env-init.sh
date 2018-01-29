562e3a2b284b7114b00b6c06301e46022db123fe
Rafael Benevides <rafabene@gmail.com>
2018-01-29 10:00:38 -0500

    Try new image openshift-middleware-3-7

--- workshop/5-fault-injection/env-init.sh
+++ workshop/5-fault-injection/env-init.sh
@@ -1,16 +1,16 @@
-ssh root@host01 "oc login -u system:admin"
+ssh root@host02 "oc login -u system:admin"
 
-ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer"
-ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
-ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system"
-ssh root@host01 "oc adm policy add-scc-to-user anyuid -z default -n istio-system"
+ssh root@host02 "oc adm policy add-cluster-role-to-user cluster-admin developer"
+ssh root@host02 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
+ssh root@host02 "oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system"
+ssh root@host02 "oc adm policy add-scc-to-user anyuid -z default -n istio-system"
 
-ssh root@host01 "wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/"
-ssh root@host01 "tar -zxvf /root/istio-0.4.0-linux.tar.gz -C /root" 
+ssh root@host02 "wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/"
+ssh root@host02 "tar -zxvf /root/istio-0.4.0-linux.tar.gz -C /root" 
 
-ssh root@host01 "oc apply -f /root/istio-0.4.0/install/kubernetes/istio.yaml"
+ssh root@host02 "oc apply -f /root/istio-0.4.0/install/kubernetes/istio.yaml"
 
-ssh root@host01 "oc expose svc istio-ingress -n istio-system"
-ssh root@host01 "oc login -u developer -p developer"
+ssh root@host02 "oc expose svc istio-ingress -n istio-system"
+ssh root@host02 "oc login -u developer -p developer"
 
-ssh root@host01 "wget https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail -P /usr/local/bin && chmod +x /usr/local/bin/kubetail"
+ssh root@host02 "wget https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail -P /usr/local/bin && chmod +x /usr/local/bin/kubetail"
