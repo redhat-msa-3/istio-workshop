c6c496cf6d0af364c4cf1c6459ebe9ee148bd116
Rafael Benevides <rafabene@gmail.com>
2018-01-30 12:51:53 -0500

    Adjust env init script to wait for openshift

--- workshop/2-deploy-microservices/env-init.sh
+++ workshop/2-deploy-microservices/env-init.sh
@@ -1,10 +1,19 @@
 ssh root@host01 "rm -rf /root/projects/ /root/temp-pom.xml"
 
-ssh root@host01 "wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/; tar -zxvf /root/istio-0.4.0-linux.tar.gz -C /root; until $(oc status &> /dev/null); do sleep 1; done; oc login -u system:admin"
+ssh root@host01 "until $(oc status &> /dev/null); do sleep 1; done; oc login -u system:admin"
+ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer"
+ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
+ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system"
+ssh root@host01 "oc adm policy add-scc-to-user anyuid -z default -n istio-system"
 
-ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer; oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system; oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system; oc adm policy add-scc-to-user anyuid -z default -n istio-system"
+ssh root@host01 "wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/"
+ssh root@host01 "tar -zxvf /root/istio-0.4.0-linux.tar.gz -C /root" 
 
 ssh root@host01 "oc apply -f /root/istio-0.4.0/install/kubernetes/istio.yaml"
 
-ssh root@host01 "oc expose svc istio-ingress -n istio-system"
+ssh root@host01 "oc project istio-system"
+
+ssh root@host01 "oc expose svc istio-ingress"
+ssh root@host01 "oc login -u developer -p developer"
+ssh root@host01 "oc project default"
 
