8598e7409ed00ff3ca2b2da8fff7ae84d2a5c7f1
Rafael Benevides <rafabene@gmail.com>
2018-01-30 13:17:39 -0500

    Adjust istio installation script

--- workshop/2-deploy-microservices/set-env.sh
+++ workshop/2-deploy-microservices/set-env.sh
@@ -1,5 +1,5 @@
 ~/.launch.sh
 export PATH=$PATH:/root/istio-0.4.0/bin/
 echo "Wait while we install Istio in this scenario"
-chmod +x /root/install-istio
+chmod +x /root/install-istio.sh
 ~/install-istio.sh
