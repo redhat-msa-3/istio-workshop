0033d8e342da163a5029a8c7893b6fa4e797fd1e
Rafael Benevides <rafabene@gmail.com>
2018-01-30 12:46:40 -0500

    Adjust env init script to wait for openshift

--- workshop/2-deploy-microservices/env-init.sh
+++ workshop/2-deploy-microservices/env-init.sh
@@ -1,7 +1,6 @@
 ssh root@host01 "rm -rf /root/projects/ /root/temp-pom.xml"
 
-ssh root@host01 "wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/"
-ssh root@host01 "tar -zxvf /root/istio-0.4.0-linux.tar.gz -C /root; until $(oc status &> /dev/null); do sleep 1; done; oc login -u system:admin" 
+ssh root@host01 "wget https://github.com/istio/istio/releases/download/0.4.0/istio-0.4.0-linux.tar.gz -P /root/; tar -zxvf /root/istio-0.4.0-linux.tar.gz -C /root; until $(oc status &> /dev/null); do sleep 1; done; oc login -u system:admin"
 
 ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer; oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system; oc adm policy add-scc-to-user anyuid -z istio-egress-service-account -n istio-system; oc adm policy add-scc-to-user anyuid -z default -n istio-system"
 
