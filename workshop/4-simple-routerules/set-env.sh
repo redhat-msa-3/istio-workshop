91093d77ad69b8172904118b824b5fce681be264
Rafael Benevides <rafabene@gmail.com>
2018-01-30 13:53:52 -0500

    Fix scripts execution

--- workshop/4-simple-routerules/set-env.sh
+++ workshop/4-simple-routerules/set-env.sh
@@ -1,7 +1,7 @@
 ~/.launch.sh
 echo "Wait while we install Istio in this scenario"
-~/install-istio.sh
 chmod +x /root/install-istio.sh
+~/install-istio.sh
 echo "Wait while we install the microservices in this scenario"
 chmod +x /root/install-microservices.sh
 ~/install-microservices.sh
