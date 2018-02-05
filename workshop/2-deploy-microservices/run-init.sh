cfa7f138a6860d0e0f7d59c1252269364cce086f
Rafael Benevides <rafabene@gmail.com>
2018-02-05 18:51:49 -0500

    Give developer permission

--- workshop/2-deploy-microservices/run-init.sh
+++ workshop/2-deploy-microservices/run-init.sh
@@ -1,4 +1,5 @@
 ssh root@host01 "rm -rf /root/projects/* /root/temp-pom.xml"
+ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer"
 ssh root@host01 "mkdir -p /root/installation"
 ssh root@host01 "wget https://github.com/istio/istio/releases/download/0.5.0/istio-0.5.0-linux.tar.gz -P /root/installation"
 ssh root@host01 "tar -zxvf /root/installation/istio-0.5.0-linux.tar.gz -C /root/installation"
