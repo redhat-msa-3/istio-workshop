c2ebb1d14108adc124c0c148ed657791d82be281
Rafael Benevides <rafabene@gmail.com>
2018-02-05 23:46:02 -0500

    Don't remove the existing project

--- workshop/3-monitoring-tracing/run-init.sh
+++ workshop/3-monitoring-tracing/run-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ssh root@host01 "rm -rf /root/projects/* /root/temp-pom.xml"
+ssh root@host01 "rm -rf /root/projects/rhoar-getting-started /root/temp-pom.xml"
 
 ssh root@host01 "sleep 20; oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
 
