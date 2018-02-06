97312f25ee390f0b9e7856c315fa5f4a892c566b
Rafael Benevides <rafabene@gmail.com>
2018-02-05 19:33:32 -0500

    Script to wait login

--- workshop/1-introduction/run-init.sh
+++ workshop/1-introduction/run-init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ssh root@host01 "rm -rf /root/projects/rhoar-getting-started/ /root/temp-pom.xml"
-ssh root@host01 "until $(oc get svc router -n default&> /dev/null); do sleep 1; done; oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
+ssh root@host01 "until $(oc status &> /dev/null); do sleep 1; done; oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
 
