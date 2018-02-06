5abd40cfb8fd495ca2ec28c1337f708217fe974c
Rafael Benevides <rafabene@gmail.com>
2018-02-05 23:42:41 -0500

    Update scenario 3

--- workshop/2-deploy-microservices/run-init.sh
+++ workshop/2-deploy-microservices/run-init.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 ssh root@host01 "rm -rf /root/projects/* /root/temp-pom.xml"
 
 ssh root@host01 "sleep 20; oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
