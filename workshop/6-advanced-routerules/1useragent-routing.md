090480a7fbfb2d693f75b754049f4397abf10615
Rafael Benevides <rafabene@gmail.com>
2018-02-06 17:29:25 -0500

    Adjust scenario 6

--- workshop/6-advanced-routerules/1useragent-routing.md
+++ workshop/6-advanced-routerules/1useragent-routing.md
@@ -8,19 +8,19 @@ To watch the creation of the pods, execute `oc get pods -w`{{execute}}
 
 Once that the recommendations pods READY column are 2/2, you can hit `CTRL+C`. 
 
-Let's create a rule that points all request to v1 using the [route-rule-recommendations-v1.yml](https://github.com/redhat-developer-demos/istio-tutorial/blob/master/istiofiles/route-rule-recommendations-v1.yml) file.
+Let's create a rule that points all request to v1 using the file `istio-tutorial/blob/master/istiofiles/route-rule-recommendation-v1.yml`{{open}}.
 
-`oc create -f ~/projects/istio-tutorial/istiofiles/route-rule-recommendations-v1.yml -n tutorial`{{execute}}
+`oc create -f ~/projects/istio-tutorial/istiofiles/route-rule-recommendation-v1.yml -n tutorial`{{execute}}
 
 Check this behaviour trying the microservice several times by typing `while true; do curl http://customer-tutorial.[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com; sleep .1; done`{{execute}}
 
 Hit CTRL+C when you are satisfied.
 
-Now check the file [route-rule-safari-recommendations-v2.yml](https://github.com/redhat-developer-demos/istio-tutorial/blob/master/istiofiles/route-rule-safari-recommendations-v2.yml).
+Now check the file `istio-tutorial/blob/master/istiofiles/route-rule-safari-recommendation-v2.yml`{{open}}.
 
 Note that this `RouteRule` will only route request to `recommendations` that contains the label `version=v2` when the `request` contains a `header` where the `user-agent` value `matches` the `regex` expression to `".*Safari.*"`.
 
-Let's apply this rule: `oc create -f ~/projects/istio-tutorial/istiofiles/route-rule-safari-recommendations-v2.yml -n tutorial`{{execute}}
+Let's apply this rule: `oc create -f ~/projects/istio-tutorial/istiofiles/route-rule-safari-recommendation-v2.yml -n tutorial`{{execute}}
 
 Now test the URL http://customer-tutorial.[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com with a Safari (or even Chrome on Mac since it includes Safari in the string). Safari only sees v2 responses from recommendations
 
@@ -35,12 +35,12 @@ For example. Try `curl -A Safari http://customer-tutorial.[[HOST_SUBDOMAIN]]-80-
 
 Alternatively you can try a `Firefox` user-agent with `curl -A Firefox http://customer-tutorial.[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com`{{execute}}
 
-You can describe the routerule to see its configuration: `oc describe routerule recommendations-safari -n tutorial`{{execute}} 
+You can describe the routerule to see its configuration: `oc describe routerule recommendation-safari -n tutorial`{{execute}} 
 
 ## Remove 'Safari' rule.
 
-To remove the User-Agent behaviour, simply delete this `routerule` by executing `oc delete routerule recommendations-safari -n tutorial`{{execute}}
+To remove the User-Agent behaviour, simply delete this `routerule` by executing `oc delete routerule recommendation-safari -n tutorial`{{execute}}
 
 To check if you have all requests using `v1`, try the microservice several times by typing `curl http://customer-tutorial.[[HOST_SUBDOMAIN]]-80-[[KATACODA_HOST]].environments.katacoda.com`{{execute}}
 
-You still have the requests going to `v1` because you didn't remove the RouteRule `recommendations-default`.
+You still have the requests going to `v1` because you didn't remove the RouteRule `recommendation-default`.
--- workshop/6-advanced-routerules/assets/install-microservices.sh
+++ workshop/6-advanced-routerules/assets/install-microservices.sh
@@ -1,43 +1,29 @@
 #!/bin/bash
-echo "Cloning project's git repository"
-mkdir -p /root/projects/istio-tutorial
-git --git-dir=/root/projects/istio-tutorial/.git pull || { echo "Error running git pull"; exit 1; }
-cp -Rvf /root/projects/istio-tutorial/recommendations/ /root/projects/istio-tutorial/recommendations-v2 &> /dev/null
-git apply /root/recommendations-v2.diff --directory=/root/projects/istio-tutorial/ || { echo "Git apply failed. Check file recommendations-v2.diff"; exit 1; }
-rm /root/recommendations-v2.diff
+git --git-dir=/root/projects/istio-tutorial/.git pull
+cp -Rvf /root/projects/istio-tutorial/recommendation/ /root/projects/istio-tutorial/recommendation-v2
+git apply /root/recommendation-v2.diff --directory=/root/projects/istio-tutorial/
+
+oc login -u system:admin
+
 oc new-project tutorial
 oc adm policy add-scc-to-user privileged -z default -n tutorial
+mvn package -f /root/projects/istio-tutorial/customer/ -DskipTests
+mvn package -f /root/projects/istio-tutorial/preference/ -DskipTests
+mvn package -f /root/projects/istio-tutorial/recommendation/ -DskipTests
+mvn package -f /root/projects/istio-tutorial/recommendation-v2/ -DskipTests
 
-#Package Projects
-echo "Building customer microservice with Maven. First Maven build takes a while"
-mvn package -f /root/projects/istio-tutorial/customer/ -DskipTests &> /dev/null || { echo "Error building customer"; exit 1; }
-echo "Building preferences microservice with Maven"
-mvn package -f /root/projects/istio-tutorial/preferences/ -DskipTests &> /dev/null || { echo "Error building preferences"; exit 1; }
-echo "Building recommendations microservice with Maven"
-mvn package -f /root/projects/istio-tutorial/recommendations/ -DskipTests &> /dev/null || { echo "Error building recommendations"; exit 1; }
-echo "Building recommendations-v2 microservice with Maven"
-mvn package -f /root/projects/istio-tutorial/recommendations-v2/ -DskipTests &> /dev/null || { echo "Error building recommendations-v2"; exit 1; }
+docker build -t example/customer /root/projects/istio-tutorial/customer/
+docker build -t example/preference /root/projects/istio-tutorial/preference/
+docker build -t example/recommendation:v1 /root/projects/istio-tutorial/recommendation/
+docker build -t example/recommendation:v2 /root/projects/istio-tutorial/recommendation-v2/
 
-#docker build
-echo "Building customer docker image. First docker build takes a while"
-docker build -t example/customer /root/projects/istio-tutorial/customer/ &> /dev/null || { echo "Error building customer docker image"; exit 1; }
-echo "Building preferences docker image"
-docker build -t example/preferences /root/projects/istio-tutorial/preferences/ &> /dev/null || { echo "Error building preferences docker image"; exit 1; }
-echo "Building recommendations docker image"
-docker build -t example/recommendations:v1 /root/projects/istio-tutorial/recommendations/ &> /dev/null || { echo "Error building recommendations docker image"; exit 1; }
-echo "Building recommendations-v2 docker image"
-docker build -t example/recommendations:v2 /root/projects/istio-tutorial/recommendations-v2/ &> /dev/null || { echo "Error building recommendations-v2 docker image"; exit 1; }
-
-#Deploy to OpenShift
 oc apply -f <(/root/installation/istio-0.5.0/bin/istioctl kube-inject -f /root/projects/istio-tutorial/customer/src/main/kubernetes/Deployment.yml) -n tutorial
-oc apply -f <(/root/installation/istio-0.5.0/bin/istioctl kube-inject -f /root/projects/istio-tutorial/preferences/src/main/kubernetes/Deployment.yml) -n tutorial
-oc apply -f <(/root/installation/istio-0.5.0/bin/istioctl kube-inject -f /root/projects/istio-tutorial/recommendations/src/main/kubernetes/Deployment.yml) -n tutorial
-oc apply -f <(/root/installation/istio-0.5.0/bin/istioctl kube-inject -f /root/projects/istio-tutorial/kubernetesfiles/recommendations_v2_deployment.yml) -n tutorial
+oc apply -f <(/root/installation/istio-0.5.0/bin/istioctl kube-inject -f /root/projects/istio-tutorial/preference/src/main/kubernetes/Deployment.yml) -n tutorial
+oc apply -f <(/root/installation/istio-0.5.0/bin/istioctl kube-inject -f /root/projects/istio-tutorial/recommendation/src/main/kubernetes/Deployment.yml) -n tutorial
+oc apply -f <(/root/installation/istio-0.5.0/bin/istioctl kube-inject -f /root/projects/istio-tutorial//kubernetesfiles/recommendation_v2_deployment.yml) -n tutorial
 
 oc create -f /root/projects/istio-tutorial/customer/src/main/kubernetes/Service.yml -n tutorial
-oc create -f /root/projects/istio-tutorial/preferences/src/main/kubernetes/Service.yml -n tutorial
-oc create -f /root/projects/istio-tutorial/recommendations/src/main/kubernetes/Service.yml -n tutorial
+oc create -f /root/projects/istio-tutorial/preference/src/main/kubernetes/Service.yml -n tutorial
+oc create -f /root/projects/istio-tutorial/recommendation/src/main/kubernetes/Service.yml -n tutorial
 
 oc expose service customer -n tutorial
-echo "All 4 microservices (customer, preferences, recommendations-v1 and recommendations-v2) have been deployed"
-
