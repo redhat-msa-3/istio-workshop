f29d6e2f2d99a3958d5f053d9970df8519e9b56f
Rafael Benevides <rafabene@gmail.com>
2018-02-06 00:17:21 -0500

    Wait of istio-system availability

--- workshop/3-monitoring-tracing/run-init.sh
+++ workshop/3-monitoring-tracing/run-init.sh
@@ -6,7 +6,7 @@ ssh root@host01 "mkdir -p /root/installation"
 ssh root@host01 "wget https://github.com/istio/istio/releases/download/0.5.0/istio-0.5.0-linux.tar.gz -P /root/installation"
 ssh root@host01 "tar -zxvf /root/installation/istio-0.5.0-linux.tar.gz -C /root/installation"
 
-ssh root@host01 "oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
+ssh root@host01 "sleep 10; oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
 
 ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer"
 ssh root@host01 "oc adm policy add-scc-to-user anyuid -z istio-ingress-service-account -n istio-system"
