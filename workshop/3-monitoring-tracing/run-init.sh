55f27997ebba8d7b2fdd9b7bb311f1faf45aceeb
Rafael Benevides <rafabene@gmail.com>
2018-02-06 17:58:28 -0500

    Tweak on wget

--- workshop/3-monitoring-tracing/run-init.sh
+++ workshop/3-monitoring-tracing/run-init.sh
@@ -3,7 +3,7 @@ ssh root@host01 "rm -rf /root/projects/rhoar-getting-started /root/temp-pom.xml"
 
 
 ssh root@host01 "mkdir -p /root/installation"
-ssh root@host01 "wget https://github.com/istio/istio/releases/download/0.5.0/istio-0.5.0-linux.tar.gz -P /root/installation"
+ssh root@host01 "wget -c https://github.com/istio/istio/releases/download/0.5.0/istio-0.5.0-linux.tar.gz -P /root/installation"
 ssh root@host01 "tar -zxvf /root/installation/istio-0.5.0-linux.tar.gz -C /root/installation"
 
 ssh root@host01 "sleep 10; oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
