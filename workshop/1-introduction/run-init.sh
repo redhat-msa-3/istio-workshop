58c7b74b6edb372a0d9bbed92418c5025802eb2e
Rafael Benevides <rafabene@gmail.com>
2018-02-05 19:01:52 -0500

    Update background script

--- workshop/1-introduction/run-init.sh
+++ workshop/1-introduction/run-init.sh
@@ -1,1 +1,3 @@
 ssh root@host01 "rm -rf /root/projects/rhoar-getting-started/ /root/temp-pom.xml"
+ssh root@host01 "oc login -u system:admim"
+ssh root@host01 "oc adm policy add-cluster-role-to-user cluster-admin developer"
