d3224df5026086026b3874a474da56539dc2796e
Rafael Benevides <rafabene@gmail.com>
2018-02-19 18:48:22 -0500

    Fix script

--- workshop/1-introduction/run-init.sh
+++ workshop/1-introduction/run-init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ssh root@host01 "git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tutorial/.git pull"
 ssh root@host01 "rm -rf /root/projects/* /root/temp-pom.xml"
-ssh root@host01 "until $(oc get project istio-system &> /dev/null); do sleep 1; done; oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
 
--- workshop/1-introduction/set-env.sh
+++ workshop/1-introduction/set-env.sh
@@ -1,2 +1,3 @@
 ~/.launch.sh
+oc adm policy add-cluster-role-to-user cluster-admin admin
 mkdir -p /root/installation && cd ~/installation
