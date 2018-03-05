fb6a1b181fc2da89505cf1e63f64d679c73d6dfd
Rafael Benevides <rafabene@gmail.com>
2018-03-05 18:43:48 -0500

    Adjust script

--- workshop/2-deploy-microservices/run-init.sh
+++ workshop/2-deploy-microservices/run-init.sh
@@ -3,8 +3,6 @@ ssh root@host01 "rm -rf /root/projects/* /root/temp-pom.xml"
 
 ssh root@host01 "wget -c https://github.com/istio/istio/releases/download/0.6.0/istio-0.6.0-linux.tar.gz -P /root/installation"
 
-ssh root@host01 "git --work-tree=/root/projects/istio-tutorial/ --git-dir=/root/projects/istio-tutorial/.git pull"
-
 ssh root@host01 "tar -zxvf /root/installation/istio-0.6.0-linux.tar.gz -C /root/installation"
 
 ssh root@host01 "sleep 10; oc login -u system:admin; oc adm policy add-cluster-role-to-user cluster-admin admin"
